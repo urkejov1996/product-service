786a3bed2ecf39105e0f3a912354eb0613c6aa16
urkejov1996 <urkejov1996@gmail.com>
2024-03-30 11:36:44 +0100

    added integration tests for create product

--- src/main/java/com/urkejov/productservice/controller/ProductController.java
+++ src/main/java/com/urkejov/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.urkejov.productservice.dto.ProductResponse;
 import com.urkejov.productservice.service.ProductService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +18,13 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
